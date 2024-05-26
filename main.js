b8ef8c8fda5b3aba83efea9e536401f7319429f9
MuhammadHasan004 <hasanmuhammadhm1122@gmail.com>
2024-05-26 14:06:49 +0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,16 @@
+// addition
+var num1 = 4;
+var num2 = 4;
+console.log(num1 + num2);
+// subtraction
+var num = 9;
+var othernum = 1;
+console.log(num - othernum);
+// multiplication
+var digit1 = 2;
+var digit2 = 4;
+console.log(digit1 * digit2);
+// division
+var value1 = 16;
+var value2 = 2;
+console.log(value1 / value2);
---
+++ main.ts
@@ -0,0 +1,16 @@
+// addition
+let num1:number=4
+let num2:number=4
+console.log(num1+num2)
+// subtraction
+let num:number=9
+let othernum:number=1
+console.log(num-othernum);
+// multiplication
+let digit1:number=2;
+let digit2:number=4;
+console.log(digit1*digit2);
+// division
+let value1:number=16
+let value2:number=2;
+console.log(value1/value2);
